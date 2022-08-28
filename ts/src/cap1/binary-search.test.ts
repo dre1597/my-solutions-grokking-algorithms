a338911c977f26816984d6e75f6f2eb26347ed59
André Pereira <andrepereira97@yahoo.com>
2022-08-28 11:58:39 -0300

    :white_check_mark: should return the index of the element if its on the list

--- ts/src/cap1/binary-search.test.ts
+++ ts/src/cap1/binary-search.test.ts
@@ -16,4 +16,16 @@ describe('Binary Search', () => {
     const result = binary_search(list, elem);
     expect(result).toBe(null);
   });
+
+  test('should return the index of the element if its on the list', () => {
+    const list = [1, 2, 3, 4, 5];
+    const elem1 = 1;
+    const elem2 = 5;
+
+    const result1 = binary_search(list, elem1);
+    const result2 = binary_search(list, elem2);
+
+    expect(result1).toBe(0);
+    expect(result2).toBe(4);
+  });
 });
--- ts/src/cap1/binary-search.ts
+++ ts/src/cap1/binary-search.ts
@@ -1,3 +1,22 @@
 export const binary_search = <T>(list: T[], elem: T): number | null => {
+  let start: number = 0;
+  let end: number = list.length - 1;
+
+  while (start <= end) {
+    let middle: number = Math.floor((start + end) / 2);
+
+    let guess: T = list[middle];
+
+    if (guess === elem) {
+      return middle;
+    }
+
+    if (guess > elem) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+  }
+
   return null;
 };
