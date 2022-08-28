85d6ac48f4658b276bac2c54e3ff9d5d2d39a4f9
André Pereira <andrepereira97@yahoo.com>
2022-08-28 12:22:09 -0300

    :recycle: implements recursive binary search

--- ts/src/cap1/recursive-binary-search.ts
+++ ts/src/cap1/recursive-binary-search.ts
@@ -1,3 +1,24 @@
-export const recursiveBinarySearch = <T>(list: T[], elem: T): number | null => {
+export const recursiveBinarySearch = <T>(
+  list: T[],
+  elem: T,
+  start: number = 0,
+  end: number = list.length - 1
+): number | null => {
+  if (start <= end) {
+    let middle: number = Math.floor((start + end) / 2);
+
+    let guess: T = list[middle];
+
+    if (guess === elem) {
+      return middle;
+    }
+
+    if (guess > elem) {
+      return recursiveBinarySearch(list, elem, start, middle - 1);
+    } else {
+      return recursiveBinarySearch(list, elem, middle + 1, end);
+    }
+  }
+
   return null;
 };
