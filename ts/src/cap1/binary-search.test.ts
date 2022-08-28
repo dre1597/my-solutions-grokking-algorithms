68e63dd2fd5a98bb218ce5f8208d64a11369c3ad
André Pereira <andrepereira97@yahoo.com>
2022-08-28 12:36:42 -0300

    :recycle: combine tests

--- ts/src/cap1/binary-search.test.ts
+++ ts/src/cap1/binary-search.test.ts
@@ -1,31 +1,68 @@
 import { binarySearch } from './binary-search';
+import { recursiveBinarySearch } from './recursive-binary-search';
+
+const emptyList = [];
+const numbersList = [1, 2, 3, 4, 5];
+const charsList = ['a', 'b', 'c', 'd', 'e'];
 
 describe('Binary Search', () => {
   test('should return null if the list is empty', () => {
-    const list = [];
-    const elem = 1;
-
-    const result = binarySearch(list, elem);
+    const result = binarySearch(emptyList, 1);
     expect(result).toBe(null);
   });
 
   test('should return null if the element is not on the list', () => {
-    const list = [1, 2, 3];
-    const elem = 5;
+    const result1 = binarySearch(numbersList, 0);
+    const result2 = binarySearch(charsList, 'z');
 
-    const result = binarySearch(list, elem);
-    expect(result).toBe(null);
+    expect(result1).toBe(null);
+    expect(result2).toBe(null);
   });
 
   test('should return the index of the element if its on the list', () => {
-    const list = [1, 2, 3, 4, 5];
-    const elem1 = 1;
-    const elem2 = 5;
-
-    const result1 = binarySearch(list, elem1);
-    const result2 = binarySearch(list, elem2);
+    const result1 = binarySearch(numbersList, 1);
+    const result2 = binarySearch(numbersList, 5);
+    const result3 = binarySearch(numbersList, 3);
+    const result4 = binarySearch(charsList, 'a');
+    const result5 = binarySearch(charsList, 'e');
+    const result6 = binarySearch(charsList, 'c');
 
     expect(result1).toBe(0);
     expect(result2).toBe(4);
+    expect(result3).toBe(2);
+    expect(result4).toBe(0);
+    expect(result5).toBe(4);
+    expect(result6).toBe(2);
+  });
+});
+
+describe('Recursive Binary Search', () => {
+  test('should return null if the list is empty', () => {
+    const result = recursiveBinarySearch(emptyList, 1);
+    expect(result).toBe(null);
+  });
+
+  test('should return null if the element is not on the list', () => {
+    const result1 = recursiveBinarySearch(numbersList, 0);
+    const result2 = recursiveBinarySearch(charsList, 'z');
+
+    expect(result1).toBe(null);
+    expect(result2).toBe(null);
+  });
+
+  test('should return the index of the element if its on the list', () => {
+    const result1 = recursiveBinarySearch(numbersList, 1);
+    const result2 = recursiveBinarySearch(numbersList, 5);
+    const result3 = recursiveBinarySearch(numbersList, 3);
+    const result4 = recursiveBinarySearch(charsList, 'a');
+    const result5 = recursiveBinarySearch(charsList, 'e');
+    const result6 = recursiveBinarySearch(charsList, 'c');
+
+    expect(result1).toBe(0);
+    expect(result2).toBe(4);
+    expect(result3).toBe(2);
+    expect(result4).toBe(0);
+    expect(result5).toBe(4);
+    expect(result6).toBe(2);
   });
 });
