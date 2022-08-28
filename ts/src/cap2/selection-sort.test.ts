a7a41e4fea584943a9218513b599c8581b0bac5f
André Pereira <andrepereira97@yahoo.com>
2022-08-28 17:02:44 -0300

    :white_check_mark: should sort a list

--- ts/src/cap2/selection-sort.test.ts
+++ ts/src/cap2/selection-sort.test.ts
@@ -2,7 +2,9 @@ import { selectionSort } from './selection-sort';
 
 const emptyList = [];
 const orderedNumbersList = [1, 2, 3];
+const unorderedNumbersList = [3, 2, 1];
 const orderedCharsList = ['a', 'b', 'c'];
+const unorderedCharsList = ['c', 'b', 'a'];
 
 describe('Selection Sort', () => {
   test('should return an empty list if its called with one', () => {
@@ -15,6 +17,14 @@ describe('Selection Sort', () => {
     expect(orderedList1).toEqual(expect.arrayContaining(orderedNumbersList));
 
     const orderedList2 = selectionSort(orderedCharsList);
+    expect(orderedList2).toEqual(expect.arrayContaining(orderedCharsList));
+  });
+
+  test('should sort a list', () => {
+    const orderedList1 = selectionSort(unorderedNumbersList);
+    expect(orderedList1).toEqual(expect.arrayContaining(orderedNumbersList));
+
+    const orderedList2 = selectionSort(unorderedCharsList);
     expect(orderedList2).toEqual(expect.arrayContaining(orderedCharsList));
   });
 });
