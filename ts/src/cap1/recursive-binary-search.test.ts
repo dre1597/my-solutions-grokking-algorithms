1db3650da60d02a98e4ec676eb9877b662b25d3a
André Pereira <andrepereira97@yahoo.com>
2022-08-28 12:22:19 -0300

    :white_check_mark: should return the index of the element if its on the list

--- ts/src/cap1/recursive-binary-search.test.ts
+++ ts/src/cap1/recursive-binary-search.test.ts
@@ -16,4 +16,16 @@ describe('Binary Search Recursive', () => {
     const result = recursiveBinarySearch(list, elem);
     expect(result).toBe(null);
   });
+
+  test('should return the index of the element if its on the list', () => {
+    const list = [1, 2, 3, 4, 5];
+    const elem1 = 1;
+    const elem2 = 5;
+
+    const result1 = recursiveBinarySearch(list, elem1);
+    const result2 = recursiveBinarySearch(list, elem2);
+
+    expect(result1).toBe(0);
+    expect(result2).toBe(4);
+  });
 });
