9dea3afb830192522cf0ca0cdc2a7a8e548dc6cd
André Pereira <andrepereira97@yahoo.com>
2022-08-28 16:50:20 -0300

    :white_check_mark: should return an ordered list if its called with one

--- ts/src/cap2/selection-sort.test.ts
+++ ts/src/cap2/selection-sort.test.ts
@@ -1,8 +1,20 @@
 import { selectionSort } from './selection-sort';
 
+const emptyList = [];
+const orderedNumbersList = [1, 2, 3];
+const orderedCharsList = ['a', 'b', 'c'];
+
 describe('Selection Sort', () => {
-  test('should return an empty array if its called with one', () => {
-    const result = selectionSort([]);
-    expect(result).toEqual([]);
+  test('should return an empty list if its called with one', () => {
+    const result = selectionSort(emptyList);
+    expect(result).toEqual(emptyList);
+  });
+
+  test('should return an ordered list if its called with one', () => {
+    const orderedList1 = selectionSort(orderedNumbersList);
+    expect(orderedList1).toEqual(expect.arrayContaining(orderedNumbersList));
+
+    const orderedList2 = selectionSort(orderedCharsList);
+    expect(orderedList2).toEqual(expect.arrayContaining(orderedCharsList));
   });
 });
