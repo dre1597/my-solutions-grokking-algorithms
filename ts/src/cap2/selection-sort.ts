96d3d34dc477112fc8a0968e62bf6601e8cf1d75
André Pereira <andrepereira97@yahoo.com>
2022-08-28 16:49:51 -0300

    :sparkles: implements selection sort function

--- ts/src/cap2/selection-sort.ts
+++ ts/src/cap2/selection-sort.ts
@@ -1,5 +1,13 @@
+import { findSmallestIndex } from './find-smallest-index';
+
 export const selectionSort = <T>(list: T[]): T[] => {
   const sortedList: T[] = [];
+  const listLength = list.length;
+
+  for (let i: number = 0; i < listLength; i++) {
+    const smallestIndex: number = findSmallestIndex(list);
+    sortedList.push(list.splice(smallestIndex, 1)[0]);
+  }
 
   return sortedList;
 };
