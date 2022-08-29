68ae92b7f6118887713c28d68360f75da8fd84e2
André Pereira <andrepereira97@yahoo.com>
2022-08-29 09:01:24 -0300

    :white_check_mark: should return the sum of the elements of the list

--- ts/src/cap4/sum.test.ts
+++ ts/src/cap4/sum.test.ts
@@ -2,6 +2,9 @@ import { sum } from './sum';
 
 const emptyList = [];
 const oneElementList = [1];
+const list1 = [1, 2, 3];
+const list2 = [-1, -2, -3];
+const list3 = [-1, 1, 0];
 
 describe('Sum', () => {
   test('should return null if its called with a empty list', () => {
@@ -9,8 +12,19 @@ describe('Sum', () => {
     expect(result).toBeNull();
   });
 
-  test('Should return the only element if its called with a list with just one element', () => {
+  test('should return the only element if its called with a list with just one element', () => {
     const result = sum(oneElementList);
     expect(result).toBe(1);
+  });
+
+  test('should return the sum of the elements of the list', () => {
+    const result1 = sum(list1);
+    expect(result1).toBe(6);
+
+    const result2 = sum(list2);
+    expect(result2).toBe(-6);
+
+    const result3 = sum(list3);
+    expect(result3).toBe(0);
   });
 });
