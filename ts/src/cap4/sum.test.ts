3c3e8fe9d6ddc0f7f4ba81fd98ac631153085203
André Pereira <andrepereira97@yahoo.com>
2022-08-29 08:53:11 -0300

    :white_check_mark: should return the only element if its called with a list with just one element

--- ts/src/cap4/sum.test.ts
+++ ts/src/cap4/sum.test.ts
@@ -1,10 +1,16 @@
 import { sum } from './sum';
 
 const emptyList = [];
+const oneElementList = [1];
 
 describe('Sum', () => {
   test('should return null if its called with a empty list', () => {
     const result = sum(emptyList);
     expect(result).toBeNull();
+  });
+
+  test('Should return the only element if its called with a list with just one element', () => {
+    const result = sum(oneElementList);
+    expect(result).toBe(1);
   });
 });
